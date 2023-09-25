35986647548998890cb1cde8fc04a57e0762fcaa
Avdan Aščić <117448754+avdan-ascic@users.noreply.github.com>
2023-09-25 12:38:04 +0200

    Update config.js

--- server/src/config.js
+++ server/src/config.js
@@ -1,10 +1,10 @@
 import "dotenv/config";
 
 const config = {
-  port: process.env.PORT,
-  jwt_secret: process.env.JWT_SECRET,
-  mongo: process.env.MONGO,
-  session_secret: process.env.SESSION_SECRET,
+  port: process.env.PORT || 5000,
+  jwt_secret: process.env.JWT_SECRET || "EnterJWTSecretHere!",
+  mongo: process.env.MONGO || "mongodb://127.0.0.1/paragon-blog",
+  session_secret: process.env.SESSION_SECRET || "EnterSessionSecretHere!",
 };
 
 export default config;
